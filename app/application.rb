725e08060e3d07d35088f4d64b4dd8e75108c211
Anthony Tilelli <anthony@tilelli.me>
2020-01-17 05:03:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,6 +8,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
+        resp.status = 200
         resp.write(item.price)
       else 
         resp.status = 400
