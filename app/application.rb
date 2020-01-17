14643938f3a2040362dcb52816a36399f8d744e7
Anthony Tilelli <anthony@tilelli.me>
2020-01-17 05:02:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,12 +5,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
- 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
-        resp.write item.price
+        resp.write(item.price)
       else 
         resp.status = 400
         resp.write "Item not found"
