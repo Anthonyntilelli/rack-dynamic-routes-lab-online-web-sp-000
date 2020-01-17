5428a86fcd0ba6519816c49b85010ce430bb694c
Anthony Tilelli <anthony@tilelli.me>
2020-01-17 05:04:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,7 @@ class Application
   @@items = [Item.new("foodA",9.99)]
  
   def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+   resp, req = Rack::Response.new, Rack::Request.new(env)
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
@@ -20,5 +19,4 @@ class Application
     end
     resp.finish
   end
-
 end
